1f6fd384b309fee92b97c99e2243a55912bd0814
ThyagOliveira <taothyago@gmail.com>
2023-04-05 23:05:22 +0100

    :white_check_mark: Add tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,14 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prismaService: PrismaService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findUnique: jest.fn(),
+            },
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+            verifyAsync: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
+    prismaService = module.get<PrismaService>(PrismaService);
+    jwtService = module.get<JwtService>(JwtService);
     service = module.get<AuthService>(AuthService);
   });
 
--- src/components/components.controller.spec.ts
+++ src/components/components.controller.spec.ts
@@ -1,14 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ComponentsController } from './components.controller';
 import { ComponentsService } from './components.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Component } from '@prisma/client';
 
 describe('ComponentsController', () => {
   let controller: ComponentsController;
+  let prismaService: PrismaService;
+
+  const mockComponent: Component = {
+    id: '1',
+    title: 'Test Component',
+    category: 'Test Category',
+    description: 'Test Description',
+    urlThumbnail: 'Test url',
+    price: 9.99,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  const mockPrismaService = {
+    component: {
+      create: jest.fn((data) => Promise.resolve(mockComponent)),
+      findMany: jest.fn(() => Promise.resolve([mockComponent])),
+      findUnique: jest.fn(() => Promise.resolve(mockComponent)),
+      update: jest.fn((params) => Promise.resolve(mockComponent)),
+      delete: jest.fn(() => Promise.resolve(mockComponent)),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ComponentsController],
-      providers: [ComponentsService],
+      providers: [
+        ComponentsService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
     controller = module.get<ComponentsController>(ComponentsController);
